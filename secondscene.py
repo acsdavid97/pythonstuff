675d330b1f06a4aa119eb5fad7408cbae9386251
Acs David <acs_david97@yahoo.com>
2016-06-21 13:09:56 +0300

    game working, separate files

--- secondscene.py
+++ secondscene.py
@@ -1,9 +1,15 @@
-class secondscene(gamemap):
+class secondscene(object):
     def start(self):
         print "say a number bigger than 5"
-        answer = int(raw_input("> "))
+        try:
+            answer = int(raw_input("> "))
+        except ValueError:
+            print """we teleport you back to the first scene, since you do not know what a number is
+                OR you deliberately want to sabotage my program, but I got exception handling HA
+                """
+            return "first"
         if answer > 5:
-            print "good boy, you can go to scene 3"
-            return "third"
+            print "good boy, you can go to the final scene"
+            return "final"
         else:
             return "first"
